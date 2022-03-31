a3c2eca36b022cde1bbece66c5a917bda52f27f7
rickpallow <captainmstar@gmail.com>
2022-03-30 22:25:47 -0700

    Added CAN messaging with socket and testing code

--- Cluster_DataMonitoring_Runner_03242022.py
+++ Cluster_DataMonitoring_Runner_03242022.py
@@ -13,10 +13,14 @@ import csv
 import time
 from multiprocessing import Process
 from apscheduler.schedulers.background import BackgroundScheduler
-import NetServer
+from NetServer import NetServer
+from NetServer import Message
+import pandas as pd
+import struct
 
 # Defines the paths to the dbc files for different electric components (FrontEDU, Shifter, and BMS)
-path_to_dbc_FrontEDU = path.abspath(path.join(path.dirname(__file__), 'dbc', 'CANcommunicationProtocol_V1.7_20200803.dbc'))
+path_to_dbc_FrontEDU = path.abspath(
+    path.join(path.dirname(__file__), 'dbc', 'CANcommunicationProtocol_V1.7_20200803.dbc'))
 path_to_dbc_Shifter = path.abspath(path.join(path.dirname(__file__), 'dbc', 'Arens.dbc'))
 path_to_dbc_BMS = path.abspath(path.join(path.dirname(__file__), 'dbc', 'INDI_BMS_v2.dbc'))
 
@@ -25,51 +29,54 @@ vectorInterface = True
 kvaserInterface = False
 
 # Defines the parameter the vehicle paramters
-gearRatio =10.2
-wheelsDiameter = 744*0.001   #in meter
-currentTime = time.time()
-
-# Defines initial values
-currentTime = time.time()
+gearRatio = 10.2
+wheelsDiameter = 744 * 0.001  # in meter
 
 lock = threading.Lock()
 
+TEST = True
+
+
 class SendCANThread(threading.Thread):
-    #Creates an instance (instrument) of a CAN data Reader
+    # Creates an instance (instrument) of a CAN data Reader
     def __init__(self, instrument):
         threading.Thread.__init__(self)
         self.instrument = instrument
 
+
 class Cluster(can.Listener):
-    
-    
+    currentTime = time.time()
+    last_time = time.time()
+    last_speed = 0.0
 
     # Creates an instance of a cluster 
-    def __init__(self, channel, gearRatio, wheelsDiameter):
+    def __init__(self, channel, gearRatio, wheelsDiameter, net: NetServer):
         # Defines the can interface that we are using
-        self.bus = can.interface.Bus(bustype='vector', channel='1', bitrate=500000)
-        #self.bus = can.interface.Bus(bustype='kvaser', channel='0', bitrate=500000)
-        print('Connected to the CAN successfully!')
-        # Reads the dbc files using Pyhton CAN library
-        self.db_FrontEDU = cantools.database.load_file(path_to_dbc_FrontEDU)
-        self.db_Shifter = cantools.database.load_file(path_to_dbc_Shifter)
-        self.db_BMS = cantools.database.load_file(path_to_dbc_BMS)
-        self.notifier = can.Notifier(self.bus, [self])
+        if not TEST:
+            self.bus = can.interface.Bus(bustype='vector', channel='1', bitrate=500000)
+            # self.bus = can.interface.Bus(bustype='kvaser', channel='0', bitrate=500000)
+            print('Connected to the CAN successfully!')
+            # Reads the dbc files using Pyhton CAN library
+            self.db_FrontEDU = cantools.database.load_file(path_to_dbc_FrontEDU)
+            self.db_Shifter = cantools.database.load_file(path_to_dbc_Shifter)
+            self.db_BMS = cantools.database.load_file(path_to_dbc_BMS)
+            self.notifier = can.Notifier(self.bus, [self])
         self.threadActive = True
         self.RPM = 0
         self.vehicleSpeed = 0
-        self.shifter  = 0
+        self.shifter = 0
         self.batteryVoltage = 0
         self.batterySOC = 0
         self.batteryMode = 'DisConnected'
-        self.gearRatio =10.2
-        self.wheelsDiameter = 744*0.001   #in meter       
+        self.gearRatio = 10.2
+        self.wheelsDiameter = 744 * 0.001  # in meter
+        self.net_server = net  # socket connection to VR game and cluster
 
     def start(self):
         # Starts the Clsuer
         self.xmitThread = SendCANThread(self)
         self.xmitThread.start()
-        
+
     def stop(self):
         # Ends the cluster session
         print("Stopping Cluster monitoring data")
@@ -85,66 +92,118 @@ class Cluster(can.Listener):
             MCU_ActTorq = message_FrontEDU.decode(msg.data)['MCU_ActTorq']
             MCU_General_ctRoll = message_FrontEDU.decode(msg.data)['MCU_General_ctRoll']
             self.RPM = ActRotSpd
-            self.vehicleSpeed = ((ActRotSpd*3.6*math.pi*wheelsDiameter)/(gearRatio*60))*(0.621371)
+            self.vehicleSpeed = ((ActRotSpd * 3.6 * math.pi * wheelsDiameter) / (gearRatio * 60)) * (0.621371)
         except:
             pass
-        
+
         try:
             message_Shifter = self.db_Shifter.get_message_by_frame_id(msg.arbitration_id)
             Shifter_dbc = message_Shifter.decode(msg.data)['ShifterDisplay']
             self.shifter = Shifter_dbc
         except:
             pass
-        
+
         try:
             message_BMS = self.db_BMS.get_message_by_frame_id(msg.arbitration_id)
             Voltage = message_BMS.decode(msg.data)['BatteryPackVoltage']
             self.batteryVoltage = Voltage
         except:
-           pass 
-        
+            pass
+
         try:
             message_BMS = self.db_BMS.get_message_by_frame_id(msg.arbitration_id)
             BMS_Mode = message_BMS.decode(msg.data)['BMS_Mode']
             self.batteryMode = BMS_Mode
         except:
-           pass 
-        
+            pass
+
         try:
             message_BMS = self.db_BMS.get_message_by_frame_id(msg.arbitration_id)
             SOC = message_BMS.decode(msg.data)['SOC']
             self.batterySOC = SOC
         except:
-           pass 
+            pass
 
     def startLog(self):
         header = ['Time', 'RPM', 'vehicleSpeed', 'shifter', 'batteryVoltage', 'batterySOC', 'batteryMode']
         # Defines name of csv file 
         self.filename = "clusterLogger.csv"
-            
+
         # writing to csv file 
-        with open(self.filename, 'w', newline='') as csvfile: 
+        with open(self.filename, 'w', newline='') as csvfile:
             # Creates a csv writer object 
-            self.csvwriter = csv.writer(csvfile)     
+            self.csvwriter = csv.writer(csvfile)
             # Writes the fields 
-            self.csvwriter.writerow(header)  
-                
-    def updatelog(self):    
-        # Writes the data rows 
-        data = [time.time()-currentTime, self.RPM, self.vehicleSpeed, self.shifter, self.batteryVoltage, self.batterySOC, self.batteryMode]
-        with open(self.filename, 'a', newline='') as csvfile:  
-            self.csvwriter = csv.writer(csvfile)    
-            self.csvwriter.writerow(data)     
+            self.csvwriter.writerow(header)
+
+    def updatelog(self):
+        # Writes the data rows
+        data = [time.time() - self.currentTime, self.RPM, self.vehicleSpeed, self.shifter, self.batteryVoltage,
+                self.batterySOC, self.batteryMode]
+        with open(self.filename, 'a', newline='') as csvfile:
+            self.csvwriter = csv.writer(csvfile)
+            self.csvwriter.writerow(data)
+        # Send CAN message to VR game and cluster via socket
+        self.send_can_message()
+
+    def send_can_message(self):
+        duration = time.time() - self.last_time
+        acceleration = get_acceleration(self.last_speed, self.vehicleSpeed, duration)
+        message = Message(duration, self.vehicleSpeed, acceleration)
+        self.net_server.send_message(message)
+        # Set last_time to now and last speed
+        self.last_time = time.time()
+        self.last_speed = self.vehicleSpeed
+
+    def updatelog_test(self):
+        # Read test data from 1 lap
+        df = pd.read_csv('OneLapTest.csv')
+
+        last_time = 0.0
+        last_speed = 0.0
+
+        try:
+            for index, row in df.iterrows():
+                # relative time from start of CAN bus
+                rel_time = row["Time (rel)"]
+                # speed of vehicle in kph
+                speed = row["IprVehSpdKph"]
+                # duration is how long vehicle has been traveling at this speed
+                duration = rel_time - last_time
+                last_time = rel_time
+                # calculate acceleration
+                acceleration = get_acceleration(last_speed, speed, duration)
+                self.vehicleSpeed = speed
+                self.send_can_message()
+                # sleep for duration to simulate time delay of real CAN message
+                time.sleep(duration)
+        except Exception as e:
+            print("Unexpected exception: " + e.message)
+
+
+# acceleration = delta kph / delta hours
+def get_acceleration(old_speed, new_speed, delta_seconds):
+    delta_velocity = abs(new_speed - old_speed)
+    delta_time = delta_seconds / 3600
+    return delta_velocity / delta_time
+
 
 if __name__ == "__main__":
+    print('Initializing the NetServer...')
+    net_server = NetServer()
     print('Initializing the Cluster...')
-    clusterRunner = Cluster(0, gearRatio, wheelsDiameter)
+    clusterRunner = Cluster(0, gearRatio, wheelsDiameter, net_server)
     clusterRunner.start()
     clusterRunner.startLog()
-    sched = BackgroundScheduler()
-    # Logs the data every 10 seconds
-    sched.add_job(clusterRunner.updatelog, 'interval', seconds=0.1)
-    sched.start()
-    print('Data is been logged into a csv file...')
+    if not TEST:
+        sched = BackgroundScheduler()
+        # Logs the data every 10 seconds
+        sched.add_job(clusterRunner.updatelog, 'interval', seconds=0.1)
+        sched.start()
+        print('Data is been logged into a csv file...')
+    else:
+        clusterRunner.updatelog_test()
     while True:
         time.sleep(1.0)
+    # Shut down server if this is killed
+    net_server.close()
---
+++ client_test.py
@@ -0,0 +1,13 @@
+import socket
+
+HOST = "localhost"  # The server's hostname or IP address
+PORT = 50021  # The port used by the server
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    # s.sendall(b"Hello, world")
+    data = s.recv(1024)
+    while True:
+        data = s.recv(1024)
+        print(f"Received {data!r}")
+    s.close()
